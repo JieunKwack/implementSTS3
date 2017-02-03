c463f7933097631b0fc4271f9bf91c74aac480c1
jieunK <ji2un0317@gmail.com>
2017-02-03 18:02:47 +0900

    modify preprocessing step

--- STS3.py
+++ STS3.py
@@ -3,35 +3,29 @@ import datetime as dt
 import numpy as np
 from preprocessing import Bound
 
-#cell size
-epsilon = 10
-sigma = 6
+# set parameter cell size
+epsilon = 21
+sigma = 0.18
 
 # read some database
-D = pd.read_csv('S&P500_08to10.csv') #dir위치 재조정 필요 or data import 필요
-# data row(index): date, columns: ticker
-D.set_index(D.date, inplace=True)
-del D['date'] # date를 index로 지정한 후, date열 제거
+D = readDataasDF('CBF\\CBF_TRAIN')
+Q = readDataasDF('CBF\\CBF_TEST')
+
 BD = Bound(D) # compute Bound
 
-COLUMN_NUM = ((BD.tmax - BD.tmin)/np.timedelta64(1,'D')).astype(int) / epsilon
-
-row = np.floor((D-BD.xmin)/sigma + 1)
-
-col = np.array([pd.to_datetime(str(x), format='%Y%m%d') for x in D.index.values])
-col = ((col - BD.tmin)/dt.timedelta(epsilon) + 1).astype(int)
-col_mat = np.repeat(np.matrix(col), row.shape[1], axis=0)
-
-number = (row-1)*COLUMN_NUM + col_mat.T
-
-result = pd.DataFrame(data=(sorted(set(number.ix[:,ticker])) for ticker in number.columns), index=number.columns)
+#preproc. translate ts to set
+COLUMN_NUM = ((BD.tmax-BD.tmin)/epsilon)
+row = (D.loc[:,D.columns!='label'] - BD.xmin)/sigma + 1
+col = ((D.columns.values[1:]-BD.tmin)/epsilon+1)
+number = (row-1).mul(COLUMN_NUM) + col
+result = np.floor(pd.DataFrame(data=(sorted(set(number.loc[element])) for element in number.index)))
 result.to_csv('TimeSeriesTranstoSet.csv')
 
 Max_jac = 0
-# query = TimeSeriesTranstoSet(q, BD, epsilon, sigma)
+# query = TimeSeriesTranstoSet(Q, BD, epsilon, sigma)
 query = set(result.loc['A'])
 for ticker in result.index:
-    #jaccard sim to query
+    #jaccard sim to query~
     target = set(result.loc[ticker])
     U = query.union(target)
     I = query.intersection(target)
--- preprocessing.py
+++ preprocessing.py
@@ -2,14 +2,33 @@ import pandas as pd
 import numpy as np
 
 class Bound:
-    def __init__(self,D):
-        self.tmax = pd.to_datetime(str(D.index.max()), format='%Y%m%d') + np.timedelta64(3, 'D') # 나눠지지 않아서 임의적으로 3만큼 더 키움
-        self.tmin = pd.to_datetime(str(D.index.min()), format='%Y%m%d')
-        self.xmax = D.max().max()
-        self.xmin = D.min().min()
+    def __init__(self, df):
+        self.tmax = df.index.max()
+        self.tmin = df.index.min()
+        self.xmax = df.max().max()
+        self.xmin = df.min().min()
+
+def readDataasDF(file_path):
+    df = pd.read_csv(file_path, header=None)
+    df.rename(columns={0: 'label'}, inplace=True)
+    return df
+# data: S&P500_08to10, row(index): date, columns: ticker
+# del D['date'] # date를 index로 지정한 후, date열 제거
 
 def TimeSeriesTranstoSet(q, BD, e, s):
     r = np.floor((q - BD.xmin)/s + 1)
     c = globals()['col']
     query = sorted(set((r-1) * COLUMN_NUM + c))
     return query
+
+def Trans_outQuery_to_Set(): # Alg. needs to modify to Code
+    # !Add: devide Q into Q_in and Q_out
+    Qin = TimeSeriesTranstoSet(Q_in, BD, epsilon, sigma)
+    BQ = Bound(Q_out)
+    Qout = TimeSeriesTranstoSet(Q_out, BQ, epsilon, sigma)
+    maxNumber = BD.rows * BD.columns # maximal cell ID in Bound(D)
+    # for t in Qout:
+    #     t = t + maxNumber
+    Qout = Qout + maxNumber
+    Q = set(Qin).union(set(Qout))
+    return Q

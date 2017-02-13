bdb973d6a1236e53884d2fa330e23f21978f6fad
jieunK <ji2un0317@gmail.com>
2017-02-13 21:27:30 +0900

    [fix bug] add initialize part

--- STS3.py
+++ STS3.py
@@ -5,12 +5,10 @@ from preprocessing import Bound, readDataasDF, TimeSeriesTranstoSet, QueryTranst
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import heapq as hp
-from operator import itemgetter
 
 # set parameter cell size
 sigma = 0.82
 epsilon = 76
-heap = []
 k = 1
 
 # read some database
@@ -26,36 +24,29 @@ BD = Bound(D) # compute Bound
 D_trans = TimeSeriesTranstoSet(D, BD, epsilon, sigma)
 
 errorRate = 0
-count = 0
 for q in Q.index:
     query = Q.loc[q]
-    ans = NN()
+    heap = []
+    # ans = NN()
     BQ = Bound_q(query)
     if ((BQ.xmin < BD.xmin) | (BD.xmax < BQ.xmax)):# check Query bound out of the DB bound
         Q_trans = Trans_outQuery_to_Set(query, BD, epsilon, sigma)
     else:
         Q_trans = QueryTranstoSet(query, BD, epsilon, sigma)
-    for i in D_trans.index:
+    for i in D_trans.index: #kNNpart
         Q_trans = set(filter(lambda x: x == x , Q_trans)) # remove NAN
         Target = set(filter(lambda x: x == x , D_trans.loc[i]))
         jac = Jaccard(set(Target), set(Q_trans))
         if (len(heap) == k):
-            # ans.TS = i # index: D_trans.loc[i]
-            # ans.Jac = jac
-            # ans.label = D.label[i]
-            hp.heappushpop(heap, (jac, i))
+            hp.heappushpop(heap, (jac, i, D.label[i]))
         elif (len(heap) < k):
-            # ans.TS = i
-            # ans.Jac = jac
-            # ans.label = D.label[i]
-            hp.heappush(heap, (jac, i))
+            hp.heappush(heap, (jac, i, D.label[i]))
     kNN_list = sorted(heap, reverse = True)
-    if (query.label != D.label[kNN_list[0][1]]):
+    if (query.label != kNN_list[0][2]):
         errorRate += 1
-        # print(q, ans.TS)
-        # print(int(query.label), ans.label)
-        # print("jac: ", ans.Jac)
+        # print(query.label, kNN_list[0][2])
 
+# print("finish sort: ", kNN_list)
 print("errorRate: ", errorRate / len(Q.index))
-# print(errorRate)
+print(errorRate)
 # print("count: ", count)

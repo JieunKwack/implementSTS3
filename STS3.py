974166bdabe6abde472a8b55d4c41872d67fa2d6
jieunK <ji2un0317@gmail.com>
2017-03-03 17:48:31 +0900

    edit some code about plotting

--- STS3.py
+++ STS3.py
@@ -1,6 +1,6 @@
 import csv
 import pandas as pd
-import datetime as dt
+# import datetime as dt
 import numpy as np
 from preprocessing import Bound, readDataasDF, TimeSeriesTranstoSet, QueryTranstoSet, Bound_q, NN, Trans_outQuery_to_Set, Jaccard
 import matplotlib.patches as mpatches
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 import heapq as hp
 
 # set parameter cell size
-sigma = 0.82
-epsilon = 76
+sigma = 0.82#0.18
+epsilon = 76#21
 k = 5
+styles = ['r','y','g','c','m'] # ,'k','w']
 
 # read some database
 D = readDataasDF('CinC_ECG_torso\\CinC_ECG_torso_TRAIN')
@@ -41,20 +42,26 @@ for q in Q.index:
         if (len(heap) == k):
             hp.heappushpop(heap, (jac, i, D.label[i]))
         elif (len(heap) < k):
-            hp.heappush(heap, (jac, i, D.label[i]))
+            hp.heappush(heap, (jac, i, D.label[i])) # jac, index, label
     kNN_list = sorted(heap, reverse = True)
     if (query.label != kNN_list[0][2]):
+        fig, ax = plt.subplots()
+        query.plot(legend=False, style='b', ax=ax)
+        patch = list([mpatches.Patch(color='b', label='query'+str(query.label))])
+        for i, neighbor, style in zip(range(5), kNN_list, styles):
+            D.loc[neighbor[1]].plot(legend=False, style=style, ax=ax)
+            patch += list([mpatches.Patch(color=style, label=str(neighbor[2])+':top'+str(kNN_list.index(neighbor)))])
+        plt.legend(handles=patch)
+        fName = 'figureErr\\q'+str(q)+'.png'
+        fig.savefig(fName)
         kNN_list.insert(0, query.label) #qeury.label, (jac, index, label)...
         errorList.append(kNN_list)
         errorRate += 1
-        # print(query.label, kNN_list[0][2])
 
 with open('errorList.csv','w') as csvfile:
     errWriter = csv.writer(csvfile, delimiter=',')
-    # while errorList:
     errWriter.writerows(errorList)
 
-# print("finish sort: ", kNN_list)
+
 print("errorRate: ", errorRate / len(Q.index))
 print(errorRate)
-# print("count: ", count)
